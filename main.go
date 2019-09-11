9018780b6d8f910f3ab509759f96a44acb750b45
Ubuntu <ubuntu@ip-172-31-30-194.us-east-2.compute.internal>
2019-09-11 19:20:56 +0000

    updated

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -22,8 +23,11 @@ func cidrContains(cidr, ip string) bool {
 }
 
 func main() {
-
-	fmt.Println(cidrContains("10.0.0.0/8", "10.10.10.25/32"))
-	fmt.Println(cidrContains("10.0.0.0/8", "172.16.32.54/32"))
+	cidr := flag.String("cidr", "0.0.0.0/0", "CIDR to check")
+	ip := flag.String("ip", "127.0.0.1/32", "IP to check")
+	flag.Parse()
+	fmt.Println(cidrContains(*cidr, *ip))
+	// fmt.Println(cidrContains("10.0.0.0/8", "10.10.10.25/32"))
+	// fmt.Println(cidrContains("10.0.0.0/8", "172.16.32.54/32"))
 
 }
